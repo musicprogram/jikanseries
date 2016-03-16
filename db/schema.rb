f6882ca0c2947af7d4baac15a15b7b52b68c045e
musicprogram <jahenao17@misena.edu.co>
2016-03-16 17:41:34 -0500

    funcionando postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160312153333) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -35,7 +38,7 @@ ActiveRecord::Schema.define(version: 20160312153333) do
     t.datetime "imagen_updated_at"
   end
 
-  add_index "chapters", ["season_id"], name: "index_chapters_on_season_id"
+  add_index "chapters", ["season_id"], name: "index_chapters_on_season_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.string   "user"
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20160312153333) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["movie_id"], name: "index_comments_on_movie_id"
+  add_index "comments", ["movie_id"], name: "index_comments_on_movie_id", using: :btree
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
@@ -85,7 +88,7 @@ ActiveRecord::Schema.define(version: 20160312153333) do
     t.datetime "imagen_updated_at"
   end
 
-  add_index "seasons", ["series_temp_id"], name: "index_seasons_on_series_temp_id"
+  add_index "seasons", ["series_temp_id"], name: "index_seasons_on_series_temp_id", using: :btree
 
   create_table "series_temps", force: :cascade do |t|
     t.string   "name"
@@ -124,7 +127,10 @@ ActiveRecord::Schema.define(version: 20160312153333) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "chapters", "seasons"
+  add_foreign_key "comments", "movies"
+  add_foreign_key "seasons", "series_temps"
 end
